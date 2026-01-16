36e12752b45b1b5aa17e27d5c646246accd42c69
sergehould <houlds@vaniercollege.qc.ca>
2026-01-16 12:00:23 -0500

    file

---
+++ lab_mqtt/lab_mqtt.ino
@@ -0,0 +1,61 @@
+/*
+ * Description:
+ * Template for lab3
+ * 
+ * 
+*/
+
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+// Update these with values suitable for your network.
+const char* ssid = "iot";
+const char* password = "wireless";
+const char* mqtt_server = "192.168.0.112";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+
+
+void reconnect() {
+  
+  
+}
+
+void setup() {
+
+}
+
+
+void loop() {
+      if (!client.connected()) {
+        reconnect();
+      }
+      client.loop();
+}
